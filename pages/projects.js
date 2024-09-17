a4c31339dc8cf42f700978eed5007bd74fee8480
Dion Pettaway <dion.pettaway@gmail.com>
2024-09-17 10:34:46 -0400

    Projects setup

---
+++ pages/projects.js
@@ -0,0 +1,17 @@
+import { useLocale } from "@/lib/locale";
+import Container from "@/components/Container";
+
+export default function Page404() {
+	const locale = useLocale();
+
+	return (
+		<Container>
+			<h1 className="text-5xl text-black dark:text-white text-center">
+				PROJECTS LIVE HERE
+			</h1>
+			<p className="text-xl text-gray-600 dark:text-gray-300 text-center">
+				{locale.PAGE.ERROR_404.MESSAGE}
+			</p>
+		</Container>
+	);
+}
